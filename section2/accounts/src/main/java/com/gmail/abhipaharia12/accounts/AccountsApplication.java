ebd729bd58078f08d373b3391c474a4fcf9e754d
abhishekpaharia <abhipaharia12@gmail.com>
2024-04-14 19:02:29 -0400

    created swagger documentation.

--- section2/accounts/src/main/java/com/gmail/abhipaharia12/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/gmail/abhipaharia12/accounts/AccountsApplication.java
@@ -4,8 +4,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "DummyBank Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Abhishek Paharia (abhipaharia12@gmail.com)",
+						url = "https://www.linkedin.com/in/abhishek-paharia/"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "EazyBank Accounts microservice REST API Documentation"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
