d3f86290e5b1f44f712ebbf36eb2028c6a12f4a3
abhishekpaharia <abhipaharia12@gmail.com>
2024-04-13 20:37:55 -0400

    1. created dto pattern. 2. created response and errorResponse DTO.

--- section2/accounts/src/main/java/com/gmail/abhipaharia12/accounts/controller/AccountsController.java
+++ section2/accounts/src/main/java/com/gmail/abhipaharia12/accounts/controller/AccountsController.java
@@ -1,13 +1,24 @@
 package com.gmail.abhipaharia12.accounts.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gmail.abhipaharia12.accounts.constants.AccountConstants;
+import com.gmail.abhipaharia12.accounts.dto.ResponseDto;
+
 @RestController
+@RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 public class AccountsController {
 
-    @GetMapping("/sayHello")
-    public String sayHello(){
-        return "Hello world";
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createAccount(){
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(new ResponseDto(AccountConstants.STATUS_201, AccountConstants.MESSAGE_201));
     }
 }
---
+++ section2/accounts/src/main/java/com/gmail/abhipaharia12/accounts/dto/ResponseDto.java
@@ -0,0 +1,11 @@
+package com.gmail.abhipaharia12.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class ResponseDto {
+    private String statusCode;
+
+    private String statusMsg;
+}
