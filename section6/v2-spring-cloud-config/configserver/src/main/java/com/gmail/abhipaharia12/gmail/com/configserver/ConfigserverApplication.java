ade7c126765c809c9209e840594a0da7da6d0073
abhishekpaharia <abhipaharia12@gmail.com>
2024-04-18 20:22:19 -0400

    initial setup of configserver

---
+++ section6/v2-spring-cloud-config/configserver/src/main/java/com/gmail/abhipaharia12/gmail/com/configserver/ConfigserverApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.abhipaharia12.gmail.com.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserverApplication.class, args);
+	}
+
+}
---
+++ section6/v2-spring-cloud-config/configserver/src/test/java/com/gmail/abhipaharia12/gmail/com/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.abhipaharia12.gmail.com.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
