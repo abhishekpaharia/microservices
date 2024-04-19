9fddfaca52ebbaa302bebbdcc705ed1f7bc6cde7
abhishekpaharia <abhipaharia12@gmail.com>
2024-04-18 21:04:12 -0400

    1. enabling the project to be cloud config project. 2. creating config files for all services. 3. loading all config file in the configserver using classpath.

--- section6/v2-spring-cloud-config/configserver/src/main/java/com/gmail/abhipaharia12/gmail/com/configserver/ConfigserverApplication.java
+++ section6/v2-spring-cloud-config/configserver/src/main/java/com/gmail/abhipaharia12/gmail/com/configserver/ConfigserverApplication.java
@@ -2,8 +2,10 @@ package com.gmail.abhipaharia12.gmail.com.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
