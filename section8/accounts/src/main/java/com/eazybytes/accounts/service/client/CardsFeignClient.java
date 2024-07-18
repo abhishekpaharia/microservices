da483990789b8761575d8dd972a5e66d684dca86
abhishekpaharia <abhipaharia12@gmail.com>
2024-07-18 13:26:45 -0700

    FiegnClient implementation

--- section8/accounts/src/main/java/com/eazybytes/accounts/service/client/CardsFeignClient.java
+++ section8/accounts/src/main/java/com/eazybytes/accounts/service/client/CardsFeignClient.java
@@ -1,6 +1,9 @@
 package com.eazybytes.accounts.service.client;
 
 import com.eazybytes.accounts.dto.CardsDto;
+
+import jakarta.validation.constraints.Pattern;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient("cards")
 public interface CardsFeignClient {
 
-    @GetMapping(value = "/api/fetch",consumes = "application/json")
+    @GetMapping(value = "api/fetch", consumes = "application/json")
     public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam String mobileNumber);
 
 }
--- section8/accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomersServiceImpl.java
+++ section8/accounts/src/main/java/com/eazybytes/accounts/service/impl/CustomersServiceImpl.java
@@ -3,6 +3,7 @@ package com.eazybytes.accounts.service.impl;
 import com.eazybytes.accounts.dto.AccountsDto;
 import com.eazybytes.accounts.dto.CardsDto;
 import com.eazybytes.accounts.dto.CustomerDetailsDto;
+import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.LoansDto;
 import com.eazybytes.accounts.entity.Accounts;
 import com.eazybytes.accounts.entity.Customer;
@@ -15,6 +16,8 @@ import com.eazybytes.accounts.service.ICustomersService;
 import com.eazybytes.accounts.service.client.CardsFeignClient;
 import com.eazybytes.accounts.service.client.LoansFeignClient;
 import lombok.AllArgsConstructor;
+
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -33,23 +36,20 @@ public class CustomersServiceImpl implements ICustomersService {
      */
     @Override
     public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
-        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
-                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
-        );
-        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
-                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
-        );
+        Customer customer =  customerRepository.findByMobileNumber(mobileNumber)
+                                .orElseThrow(() -> new ResourceNotFoundException("customer", "mobileNumber", mobileNumber));
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId())
+                                .orElseThrow(() -> new ResourceNotFoundException("account", "customerId", String.valueOf(customer.getCustomerId())));
 
         CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
         customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
 
-        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
-        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+        ResponseEntity<LoansDto> loansResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        customerDetailsDto.setLoansDto(loansResponseEntity.getBody());
 
-        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
-        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+        ResponseEntity<CardsDto> cardsResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDto.setCardsDto(cardsResponseEntity.getBody());
 
         return customerDetailsDto;
-
     }
 }
